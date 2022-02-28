e57c57b7c6fe02c217ee9af7929c4b2e91c8c017
issakuss <issakuss@gmail.com>
2022-02-28 12:58:51 +0900

    🔬 feat: Sound beep

--- src/mrt/mrt.py
+++ src/mrt/mrt.py
@@ -2,10 +2,14 @@ from psychopy import core
 
 from ..const import BUTTONS
 from ..ppwrapper.task import Task
+from .sound import Beep
 
 
 class MRT(Task):
     def _run_task_head(self):
+        params = self.cfg.odd_hz, self.cfg.beep_dursec, self.cfg.use_ppsound
+        self.beep = dict(odd=Beep(*params), normal=Beep(*params))
+
         super()._run_task_head()
         self.display.disp_text('Press any key to start.')
         self.button.wait_key(keys=None)
@@ -13,14 +17,17 @@ class MRT(Task):
         self.button.clear()
 
     def _run_trial(self, stim: str):
-        super()._run_trial()
+        dursec_trial = sum([self.cfg.itvl_sec_pre,
+                            self.cfg.beep_dursec,
+                            self.cfg.itvl_sec_post])
 
+        super()._run_trial()
         while self.timer.trial.getTime() <= self.cfg.itvl_sec_pre:
-            core.wait(.1)
+            core.wait(self.button.itvl_input)
             self._get_press()
         self._present_beep(odd=int(stim) > 0, dursec=self.cfg.beep_dursec)
-        while self.timer.trial.getTime() <= self.cfg.itvl_sec_post:
-            core.wait(.1)
+        while self.timer.trial.getTime() <= dursec_trial:
+            core.wait(self.button.itvl_input)
             self._get_press()
 
     def _get_press(self):
@@ -33,3 +40,4 @@ class MRT(Task):
     def _present_beep(self, odd: bool, dursec: float):
         type = 'odd' if odd else 'normal'
         self.logger.info(f'--- Present {type} stim {dursec} sec.')
+        self.beep[type].play()
