bfdc9f8d390d916828da10572a28837c3f021ad2
issakuss <issakuss@gmail.com>
2022-03-07 16:08:04 +0900

    🔬 fix: Make proper save directory
    Fix:
    When input ID is not "s3xxx" and re-input new ID,
    dicrectory based on first incorrect ID is made.

--- src/subjectpath.py
+++ src/subjectpath.py
@@ -10,17 +10,18 @@ class _SubDir:
         self.dir_home = Path().home() / 'desktop'
 
     def ask_id(self, message):
-        sub_id = input(message)
-
-        if (not sub_id.startswith('s3')) or (len(sub_id) != 5):
-            print('ID must be s3xxx')
-            self.ask_id(message)
+        while True:
+            sub_id = input(message)
+            if (not sub_id.startswith('s3')) or (len(sub_id) != 5):
+                print('ID must be s3xxx')
+                continue
         
-        if (self.dir_home / sub_id).exists():
-            ans = input('Already exists. continue? Y/n: ')
-            if ans not in ('y', 'Y'):
-                self = self.ask_id(message)
-                return self
+            if (self.dir_home / sub_id).exists():
+                ans = input('Already exists. continue? Y/n: ')
+                if ans not in ('y', 'Y'):
+                    continue
+
+            break
 
         self.sub_id = sub_id
         self.dir = self.dir_home / sub_id
