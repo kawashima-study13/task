d9fe98c9228a34c55aa9939a7a6677ae527da4e8
issakuss <issakuss@gmail.com>
2022-03-23 17:27:21 +0900

    🔬 feat: Force run without connection

--- src/mrt/mrt.py
+++ src/mrt/mrt.py
@@ -20,11 +20,15 @@ class MRT(Task):
         def __init__(self, cfg: dict, mode: Literal['off', 'serial', 'xid']):
             if mode == 'off':
                 self.writer = self.DammyWriter()
+            try:
             if mode == 'serial':
                 self.writer = Serial(cfg.comname, dursec=cfg.pulse_dursec)
             if mode == 'xid':
                 from ..tool.xid import XID  # error if driver is not installed
                 self.writer = XID(pulse_dursec=cfg.pulse_dursec)
+            except:
+                input('Connection error, press Enter to continue.')
+                self.writer = self.DammyWriter()
 
         def write(self, code):
             if (CODES_TO_LOG is None) or (code in CODES_TO_LOG):
