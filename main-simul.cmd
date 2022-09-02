928525342e0bf47dc4e405dcf04d514963de783b
issakuss <issakuss@gmail.com>
2022-09-02 14:09:26 +0900

    🔬 params: Set parameters for DBI room

--- main-simul.cmd
+++ main-simul.cmd
@@ -1,4 +1,6 @@
-call C:\tools\miniconda3\Scripts\activate.bat
-call activate s13t
+call C:\Users\dbi\anaconda3\Scripts\activate.bat
+call activate mrt
+start progbar.cmd
+mode con lines=30 cols=90
 call python main.py
 pause
